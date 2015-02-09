6ca4e43dac21dca2bc6f08c2ae8c4dd6df742ea8
ellie <ellie@asd.com>
2015-02-09 23:31:53 +0800

    message

---
+++ www/js/ionicapp.js
@@ -0,0 +1,14 @@
+angular.module('TakeTask', ['ionic'])
+.controller('MyController', function($scope, $http) {
+  $scope.items = [1,2,3];
+  $scope.doRefresh = function() {
+    $http.get('/new-items')
+     .success(function(newItems) {
+       $scope.items = newItems;
+     })
+     .finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+  };
+});
