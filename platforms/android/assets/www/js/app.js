df97fa71d6860d006c81c24a4bc0ae24c02d76be
ellie <ellie@asd.com>
2015-04-06 13:41:33 +0800

    formvalidation

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -11,7 +11,7 @@
 			app.controller('jobMapController', jobMapController);
 			app.controller('MapViewController', MapViewController);
 			app.controller('dateController', dateController);
-			app.controller('formController', formController);
+			app.controller('popupController', popupController);
 })();
 
 
@@ -497,18 +497,66 @@ function dateController($scope){
    };
 }
 
-function formController($scope){
-
-      $scope.master = {};
-
-      $scope.update = function(user) {
-        $scope.master = angular.copy(user);
-      };
-
-      $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-      };
+function popupController($scope,$ionicPopup,$timeout){
 	
-	
+
+// Triggered on a button click, or some other target
+$scope.showPopup = function() {
+  $scope.data = {}
+
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    template: '<input type="password" ng-model="data.wifi">',
+    title: 'Enter Wi-Fi Password',
+    subTitle: 'Please use normal things',
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel' },
+      {
+        text: '<b>Save</b>',
+        type: 'button-positive',
+        onTap: function(e) {
+          if (!$scope.data.wifi) {
+            //don't allow the user to close unless he enters wifi password
+            e.preventDefault();
+          } else {
+            return $scope.data.wifi;
+          }
+        }
+      }
+    ]
+  });
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+  });
+  $timeout(function() {
+     myPopup.close(); //close the popup after 3 seconds for some reason
+  }, 3000);
+ };
+ // A confirm dialog
+ $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Consume Ice Cream',
+     template: 'Are you sure you want to eat this ice cream?'
+   });
+   confirmPopup.then(function(res) {
+     if(res) {
+       console.log('You are sure');
+     } else {
+       console.log('You are not sure');
+     }
+   });
+ };
+
+ // An alert dialog
+ $scope.showAlert = function() {
+   var alertPopup = $ionicPopup.alert({
+     title: 'Invalid Email',
+     template: 'Please enter correct email address.'
+   });
+   alertPopup.then(function(res) {
+     console.log('Thank you for not eating my delicious ice cream cone');
+   });
+ };
 }
-	
+
